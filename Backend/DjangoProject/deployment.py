a84667d27c137afcfa219348db0fff242feee3e6
Hexa-Dave <daveodafe006@gmail.com>
2024-09-24 13:23:15 +0300

    updated deployment settings

--- Backend/DjangoProject/deployment.py
+++ Backend/DjangoProject/deployment.py
@@ -1,6 +1,10 @@
 import os
 from .settings import *
-from .settings import BASE_DIR
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
